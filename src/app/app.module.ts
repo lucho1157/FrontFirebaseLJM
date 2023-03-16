a4a1ad0912fabf1f36d0edaf2f2b1d85903e926d
lucho1157 <luisgigio58@gmail.com>
2023-03-16 14:03:38 -0300

    depuración código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,15 @@ import { HamburguesaComponent } from './hamburguesa/hamburguesa.component';
 import { HabilidadesComponent } from './habilidades/habilidades.component';
 import { PortfolioService } from './portfolio.service';
 import { HttpClientModule } from '@angular/common/http';
-import { EdicionComponent } from './edicion/edicion.component';
 import { AcercademiComponent } from './acercademi/acercademi.component';
 import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { EducacionComponent } from './educacion/educacion.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
-import { EditSkilComponent } from './modals/edit-skil/edit-skil.component';
 import { FooterComponent } from './footer/footer.component';
 import { EditAcercademiComponent } from './acercademi/edit-acercademi/edit-acercademi.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NewhabilidadComponent } from './habilidades/newhabilidad/newhabilidad.component';
 import { EdithabilidadesComponent } from './habilidades/edithabilidades/edithabilidades.component';
-import { DeletehabilidadesComponent } from './habilidades/deletehabilidades/deletehabilidades.component';
 import { EditproyectosComponent } from './proyectos/editproyectos/editproyectos.component';
 import { EditeducacionComponent } from './educacion/editeducacion/editeducacion.component';
 import { NeweducacionComponent } from './educacion/neweducacion/neweducacion.component';
@@ -30,7 +27,8 @@ import { NewproyectoComponent } from './proyectos/newproyecto/newproyecto.compon
 import { EditexperienciaComponent } from './experiencia/editexperiencia/editexperiencia.component';
 import { NewexperienciaComponent } from './experiencia/newexperiencia/newexperiencia.component';
 import { EditPerfilComponent } from './acercademi/edit-perfil/edit-perfil.component';
-import { EditarExperienciaComponent } from './experiencia/editar-experiencia/editar-experiencia.component';
+
+
 
 
 
@@ -38,7 +36,6 @@ import { EditarExperienciaComponent } from './experiencia/editar-experiencia/edi
 
 const routes: Route[] = [
 { path: "login", component:LoginComponent },
-{ path: "edicion", component:EdicionComponent },
 { path: "**", redirectTo: "" }];
 
 
@@ -50,18 +47,15 @@ const routes: Route[] = [
     RedesComponent,
     HamburguesaComponent,
     HabilidadesComponent,
-    EdicionComponent,
     AcercademiComponent,
     ExperienciaComponent,
     EducacionComponent,
     ProyectosComponent,
-    EditSkilComponent,
     FooterComponent,
     EditAcercademiComponent,
     InicioComponent,
     NewhabilidadComponent,
     EdithabilidadesComponent,
-    DeletehabilidadesComponent,
     EditproyectosComponent,
     EditeducacionComponent,
     NeweducacionComponent,
@@ -69,12 +63,8 @@ const routes: Route[] = [
     EditexperienciaComponent,
     NewexperienciaComponent,
     EditPerfilComponent,
-    EditarExperienciaComponent,
-    
-    
-   
-    
   ],
+
   imports: [
     BrowserModule,
     FormsModule, ReactiveFormsModule,
